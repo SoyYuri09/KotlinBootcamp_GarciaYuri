381d81dd33dddba648d5e4e71258d3a819cf805a
SoyYuri09 <yuri.garcia252583@potros.itson.edu.mx>
2026-01-29 00:45:35 -0700

    Proyecto Hello Kotlin

--- Modulo 1/Hello Kotlin/src/Main.kt
+++ Modulo 1/Hello Kotlin/src/Main.kt
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Yuri Germán García López - 252583
 fun main() {
     fun printHello () {
         println ("Hello World")
