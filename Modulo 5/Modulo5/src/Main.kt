11414a9b02fdf89f39a58f59e94fb1a66fdd62f0
SoyYuri09 <yuri.garcia252583@potros.itson.edu.mx>
2026-01-30 16:00:00 -0700

    Creación del Módulo 5

---
+++ Modulo 5/Modulo5/src/Main.kt
@@ -0,0 +1,14 @@
+//Yuri Germán García López - 252583
+//Clase Main para probar los ejercicios del módulo 5
+fun main() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+}
